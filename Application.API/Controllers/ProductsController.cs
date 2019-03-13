f4c326f841f5e71aa839e7f19ff00dba7e6025cb
riad <riad.nazrul@gmail.com>
2019-03-14 00:48:27 +0600

    Initial work done

---
+++ Application.API/Controllers/ProductsController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Application.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Application.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        // GET: api/Product
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(ProductHelper.GetProducts());
+        }
+
+        // GET: api/Product/5
+        [HttpGet("{id}", Name = "Get")]
+        public IActionResult Get(int id)
+        {
+            return Ok(ProductHelper.GetProduct(id));
+        }
+
+        // POST: api/Product
+        [HttpPost]
+        public IActionResult Post([FromBody] Product product)
+        {
+            return Ok(ProductHelper.AddProduct(product));
+        }
+
+        // PUT: api/Product/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Product product)
+        {
+            return Ok(ProductHelper.Update(id, product));
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return Ok(ProductHelper.DeleteProduct(id));
+        }
+
+      
+    }
+}
---
+++ Application.API/Models/ProductHelper.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Application.API.Models
+{
+    public static class ProductHelper
+    {
+        public static  List<Product> Products = new List<Product>
+        {
+            new Product(){Id = 1, Name = "java how to program", Description = "It is a good book"},
+            new Product(){Id = 1, Name = "C++ how to program", Description = "It is a good book"}
+        };
+
+        public static Product AddProduct(Product product)
+        {
+            Products.Add(product);
+
+            return product;
+        }
+
+        public static Product GetProduct(int id)
+        {
+            return Products.FirstOrDefault(x => x.Id == id);
+        }
+
+        public static List<Product> GetProducts()
+        {
+            return Products.ToList();
+        }
+
+        public static Product Update(int id, Product product)
+        {
+            var Aproduct = Products.FirstOrDefault(x => x.Id == id);
+            Aproduct.Name = product.Name;
+            Aproduct.Description = product.Description;
+            return Aproduct;
+
+        }
+
+        public static List<Product> DeleteProduct(int id)
+        {
+            var products = Products.Where(x => x.Id != id).ToList();
+            Products = products;
+            return Products;
+        }
+    }
+}
